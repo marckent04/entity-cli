5f7c69b20d5618a4267ce8f6edf219ce0556b3ad
Marc-Henry <36090455+marckent04@users.noreply.github.com>
2020-10-26 19:02:55 +0000

    rootdir fix

--- core/modules/common/common.mjs
+++ core/modules/common/common.mjs
@@ -4,9 +4,9 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-// export const rootDir = path.join(__dirname, "..", "..", "..", "..", "..");
+export const rootDir = path.join(__dirname, "..", "..", "..", "..", "..");
 // const defaultDir = "."
-export const rootDir = path.join(__dirname, "..", "..", "..");
+// export const rootDir = path.join(__dirname, "..", "..", "..");
 export const defaultDirectory = path.join(rootDir, "src", "entities");
 
 export const fileExists = (name) => {
