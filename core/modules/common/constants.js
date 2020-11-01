0889d417aac1d3811068f2161f27d99f8f4fc5ad
Marc-Henry <36090455+marckent04@users.noreply.github.com>
2020-11-01 18:32:21 +0000

    prepare to publish

--- core/modules/common/constants.js
+++ core/modules/common/constants.js
@@ -1,17 +1,16 @@
 const path = require("path");
-// const rootDir = path.join(__dirname, "..", "..", "..", "..", "..");
-const rootDir = path.join(__dirname, "..", "..", "..");
+const rootDir = path.join(__dirname, "..", "..", "..", "..", "..");
+// const rootDir = path.join(__dirname, "..", "..", "..");
 
 const defaultDirectory = path.join(rootDir, "src", "entities");
 const relativeDefaultDirectory = path.join("src", "entities");
 const relativeModuleDirectory = path.join("src", "modules");
 const moduleDefaultDirectory = path.join(rootDir, relativeModuleDirectory);
 
-
 module.exports = {
-    defaultDirectory,
-    relativeDefaultDirectory,
-    moduleDefaultDirectory,
-    relativeModuleDirectory,
-    rootDir
-}
+  defaultDirectory,
+  relativeDefaultDirectory,
+  moduleDefaultDirectory,
+  relativeModuleDirectory,
+  rootDir,
+};
