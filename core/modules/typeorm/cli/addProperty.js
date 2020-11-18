0ccbf5343fbddee36181e8ea0b976472ce115738
Marc-Henry <36090455+marckent04@users.noreply.github.com>
2020-11-18 23:07:27 +0000

    add property works typeorm

--- core/modules/typeorm/cli/addProperty.js
+++ core/modules/typeorm/cli/addProperty.js
@@ -1,28 +1,17 @@
 const { inquirer } = require("../../common/cli");
 const consola = require("consola");
 const chalk = require("chalk");
-const storage = require("node-persist");
-
+const { addPropertyConstructor } = require("../../common/cli/constructors");
 const { addPropertyQuestions } = require("./questions");
 const MakeProperty = require("../makers/property");
 const addCli = require("./add");
 const EntityManager = require("../EntityManager");
 
-const cli = (entityName, arCli) =>
-  inquirer.prompt(addPropertyQuestions(entityName)).then(async (answers) => {
-    try {
-      const { name, type, add, required } = answers;
-      console.log(await storage.getItem("currentModule"));
-      EntityManager.append(
-        entityName,
-        MakeProperty[type](name, required).join("\n")
-      );
-      consola.success(chalk.green(`the ${name} column has been created `));
-      if (add) addCli(entityName, cli, arCli);
-      else consola.info(chalk.blueBright("Good code to you"));
-    } catch (error) {
-      consola.error(error);
-    }
-  });
+const cli = addPropertyConstructor({
+  entityManager: EntityManager,
+  makerProperty: MakeProperty,
+  questions: addPropertyQuestions,
+  addCli,
+});
 
 module.exports = cli;
